8c68e655bb56c762d213b669915f88ba22f3d850
user <user@163.com>
2018-10-29 19:08:35 +0800

    修改

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,5 +1,15 @@
 public class User {
     public static void main(String[] args) {
         System.out.println(999999999);
+System.out.println(999999999);
+System.out.println(999999999);
+System.out.println(999999999);
+System.out.println(999999999);
+System.out.println(999999999);
+
+System.out.println(999999999);
+System.out.println(999999999);
+System.out.println(999999999);
+
     }
 }
